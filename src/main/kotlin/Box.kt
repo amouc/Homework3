58e830bfa7a117873e905fa23507603839ae567a
amou chiang <amouc@kingbright.com>
2019-04-02 13:56:31 +0800

    0402

--- src/main/kotlin/Box.kt
+++ src/main/kotlin/Box.kt
@@ -1,33 +1,35 @@
+import com.amou.Box3Box
+import com.amou.Box5Box
 import java.util.*
-
-open class Box(var length: Double, var width: Double, var height: Double) {
-    fun validate(length: Double, width: Double, height: Double): Boolean =
-        (length <= this.length && width <= this.width && height <= this.height)
-
-}
-
-class Box3 : Box(23.0, 14.0, 13.0)
-
-class Box5 : Box(39.5, 27.5, 23.0)
 
 fun main() {
     val scanner = Scanner(System.`in`)
-    val box3 = Box3()
-    val box5 = Box5()
+    val box3 = Box3Box()
+    val box5 = Box5Box()
     println("請輸入便利箱長度:")
-    var length = scanner.nextDouble()
+    var length = scanner.nextFloat()
     println("請輸入便利箱寬度:")
-    var width = scanner.nextDouble()
+    var width = scanner.nextFloat()
     println("請輸入便利箱高度:")
-    var height = scanner.nextDouble()
-
-    if (box3.validate(length, width, height)) {
-        println("請用Box3")
-    } else if (box5.validate(length, width, height)) {
-        println("請用Box5")
-    } else {
+    var height = scanner.nextFloat()
+    if(box3.validate(length,width,height)) {
+        println("請用Box3箱子")
+    }else if(box5.validate(length,width,height)){
+        println("請用Box5箱子")
+    }else{
         println("請用更大的箱子")
     }
 
 
 }
+
+open class Box(var length:Float,var width:Float,var height:Float){
+
+    fun validate(length:Float,width:Float,height:Float):Boolean
+            =((length < this.length && width < this.width && height < this.height))
+
+}
+
+class Bpx3Box : Box(23f,14f,13f)
+
+class Box5Box : Box(39.5f,27.5f,23f)
